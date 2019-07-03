e9dfb08be45d2b6f9811b81b9a951402627c2d2f
Yining Zhang <zyn3607826@gmail.com>
2019-07-03 14:45:46 -0700

    add adoption.js

--- scripts/script.js
+++ scripts/script.js
@@ -20,19 +20,6 @@ function removeTransition(e) {
 hamburger.addEventListener("mousedown", function(e) {
   e.preventDefault();
 });
-const back_to_top_btn=document.getElementById("back-to-top-button");
 
 
-window.onscroll=function(){
-  if(window.pageYOffset>100){
-      back_to_top_btn.classList.add("show");
-  }else{
-    back_to_top_btn.classList.remove("show");
-  }
-}
-back_to_top_btn.addEventListener('click',scrollToTop);
 
-function scrollToTop(){
-  document.body.scrollTop=0;
-  document.documentElement.scrollTop=0;
-}
