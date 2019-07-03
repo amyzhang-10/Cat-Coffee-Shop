bf9aa79afb614f662048a64de826bacd03f459e8
sriranjani <sriranjani48@gmail.com>
2019-07-02 21:15:09 -0700

    Updated hamburger styles

--- scripts/script.js
+++ scripts/script.js
@@ -16,23 +16,23 @@ function removeTransition(e) {
     nav.classList.remove("activated");
   }
 }
-
 hamburger.addEventListener("mousedown", function(e) {
   e.preventDefault();
 });
-const back_to_top_btn=document.getElementById("back-to-top-button");
 
+//Back-to-top button
+const back_to_top_btn = document.getElementById("back-to-top-button");
 
-window.onscroll=function(){
-  if(window.pageYOffset>100){
-      back_to_top_btn.classList.add("show");
-  }else{
+window.onscroll = function() {
+  if (window.pageYOffset > 100) {
+    back_to_top_btn.classList.add("show");
+  } else {
     back_to_top_btn.classList.remove("show");
   }
-}
-back_to_top_btn.addEventListener('click',scrollToTop);
+};
+back_to_top_btn.addEventListener("click", scrollToTop);
 
-function scrollToTop(){
-  document.body.scrollTop=0;
-  document.documentElement.scrollTop=0;
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
